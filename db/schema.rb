19d8e07d03f06040a643d6ef3ac7f64ab9c9ee6e
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-04-16 18:22:19 -0700

    bootstrap initialized

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415235619) do
+ActiveRecord::Schema.define(version: 20150417004846) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
