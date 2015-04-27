2c080ef8cf552e3edac786c45d4f93f11d5778ac
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-04-26 19:01:51 -0700

    added a new post button to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,9 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+			redirect_to posts_path
 	end
 
 	private 
